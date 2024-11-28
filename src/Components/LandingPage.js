cb839208e6257717c2a65b326643e8c8b4fbdc40
Mariya016 <mariyakamran123@gmail.com>
2024-11-28 21:18:56 +0500

    web 7

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import '../styles/landingpage.css'; // Import CSS file
-function LandingPage() {
-return (
-<div className="landing-container">
-<h1 className="landing-title">COSMETIC CITY</h1>
-<p className="landing-subtitle">
-BEST DEALS, BEST PRODUCTS;
-</p>
-<button className="landing-button">
-Learn More
-</button>
-</div>
-);
-}
+import '../styles/landingpage.css'; // Correct CSS file import
+
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      <h className="landing-title">COSMETIC CITY</h>
+      <p1 className="landing-subtitle">BEST DEALS, BEST PRODUCTS</p1>
+      <button className="landing-button">Learn More</button>
+    </div>
+  );
+};
+
 export default LandingPage;
